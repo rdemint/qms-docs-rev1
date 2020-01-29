b6c4b53aa3c9d9f00e8f2bac86772d6258a30606
Raines <rainesdemint@gmail.com>
2020-01-29 15:04:24 -0800

    initial test is working

--- qms_docs_rev1.py
+++ qms_docs_rev1.py
@@ -1,48 +1,114 @@
-def replace_name(name_str):
-    lstr = name_str.split(' Rev')[0]
-    return lstr + ' Rev1.docx'
+import os
+import sys 
 
-def handle_doc(name_str, f_path):
-    file_type = name_str.rsplit(".")[1].lower()
-    if file_type == "pdf":
-#         os.remove(name_str+'\\'+f_path)
-        return False, name_str
-    else:
-        return True, replace_name(name_str)
+class DocRevManager(object):
+    def __init__(self):
+        self.top_dir = "C:\\Users\\raine\\Documents\\Backup\\Active QMS Documents"
+        self.rev_str = " Rev1.docx"
+        self.delete_file_types = ("pdf",)
+        self.renamed_files = []
+        self.errors = []
+        self.skipped_files = []
+        self.deleted_files = []
+        self.count = 0
+        self.main()
 
-def loop_dirs(top_dir):
-    replaced_names = []
-    paths = []
-    deleted_files = []
-    count = 0
-    for sub_dir in os.listdir(top_dir):
-        sub_dir_path = top_dir+sub_dir
-        for file_name in os.listdir(sub_dir_path):
-            replaced, result = handle_doc(file_name, sub_dir_path)
-            count += 1
-            if replaced == True:
-                paths.append(sub_dir_path + "\\" + result)
-                replaced_names.append(result)
-            else:
-                deleted_files.append(result)
-    return replaced_names, paths, deleted_files, count
+    def reset_config(self):
+        self.count = 0
+        self.renamed_files = []
+        self.skipped_files = []
+        self.deleted_files = []
+        self.errors = []
 
-def get_input():
-    print('Please input the top level directory.  Files must be placed in subdirectories of this location.')
-    top_dir = input()
-    #assert that the top_dir is valid
+    def new_line(self):
+        print("\n")
+        print("\n")
 
-def log_results(replaced_names, full_paths, deleted_names, count):
-    print("{} total replaced file names".format(replaced_names.count))
-    print("{} pdf files deleted".format(deleted_names))
-    print("{} total files expected".format(count))
+    def replace_name(self, name_str):
+        if name_str.rsplit(".")[0][-1] == "1":
+            self.skipped_files.append(name_str)
+            return None 
+        else:
+            lstr = name_str.split(' Rev')[0]
+            return lstr + self.rev_str
 
-def main():
-    top_dir = get_input()
-    replaced_names, paths, deleted_files, count = loop_dirs(top_dir)
-    log_results(replaced_names, paths, deleted_files)
+    def handle_doc(self, name_str, f_path, process=False):
+        file_type = name_str.rsplit(".")[1].lower()
+        replaced = False
+        
+        if file_type in self.delete_file_types:
+            self.deleted_files.append(name_str)
+            if process==True:
+                os.remove(os.path.join(name_str, f_path))
+        else:
+            result = self.replace_name(name_str)
+            if result is not None:
+                self.renamed_files.append(result)
+                if process==True:
+                    try:
+                        os.rename(os.path.join(f_path, name_str), os.path.join(f_path, new_name))
+                    except FileExistsError:
+                        self.skipped_files.append(name_str)
+                    except Exception as e:
+                        print(e)
 
+    def loop_dirs(self, process=False):
+        self.reset_config()
+        for sub_dir in os.listdir(self.top_dir):
+            sub_dir_path = os.path.join(self.top_dir,sub_dir)
+            for file_name in os.listdir(sub_dir_path):
+                self.handle_doc(file_name, sub_dir_path)
+                self.count+=1
+
+    def set_top_dir(self):
+        self.new_line()
+        print('Please input the top level directory or press "d" for the default.')
+        top_dir = input()
+        if top_dir == "d":
+            pass
+        else:
+            self.top_dir = top_dir
+
+    def log_results(self):
+        self.new_line()
+        total_handled = len(self.renamed_files) + len(self.deleted_files)
+        print("{} total file names modified to {}".format(len(self.renamed_files), self.rev_str))
+        print("{} files deleted".format(len(self.deleted_files)))
+        print("{} files skipped".format(len(self.skipped_files)))
+        if len(self.renamed_files) + len(self.deleted_files) + len(self.skipped_files) == self.count:
+            print("OK. All files handled.")
+        else: 
+            print("Some files were not handles or deleted correctly")
+
+    def confirm(self):
+        self.new_line()
+        total_handled = len(self.renamed_files) + len(self.deleted_files)
+        print("{} total files.".format(self.count))
+        print("{} total file names to be modified to {}.".format(len(self.renamed_files), self.rev_str))
+        print("{} files names will be skipped".format(len(self.skipped_files)))
+        self.new_line()
+        print("The final file names will be the following:")
+        for name in self.renamed_files:
+            print(name)
+        self.new_line()
+        print("{} files will be deleted".format(len(self.deleted_files)))
+        for deleted in self.deleted_files:
+            print(deleted)
+        self.new_line()
+        answer = input("Please type confirm to make changes.  Other input will exit the program.\n")
+        if answer.lower() == "confirm":
+            return True
+        else:
+            return False
+
+    def main(self):
+        self.set_top_dir()
+        self.loop_dirs()
+        if self.confirm():
+            self.loop_dirs(process=True)
+            self.log_results()
+        else:
+            sys.exit()
 
 if __name__=="__main__":
-    main()
-    
+    DocRevManager()    
