7f4b5d9e70abb84c18a952f98592327bb0cea5d6
raines <rainesdemint@gmail.com>
2020-01-30 09:00:07 -0800

    Fixed handling of xlsx files, improved spacing and logging

--- qms_docs_rev1.py
+++ qms_docs_rev1.py
@@ -4,8 +4,9 @@ import sys
 class DocRevManager(object):
     def __init__(self):
         self.top_dir = "C:\\Users\\raine\\Documents\\Backup\\Active QMS Documents"
-        self.rev_str = " Rev1.docx"
+        self.rev_str = " Rev1"
         self.delete_file_types = ("pdf",)
+        self.target_files = []
         self.renamed_files = []
         self.errors = []
         self.skipped_files = []
@@ -14,6 +15,7 @@ class DocRevManager(object):
         self.main()
 
     def reset_config(self):
+        self.target_files = []
         self.count = 0
         self.renamed_files = []
         self.skipped_files = []
@@ -22,47 +24,46 @@ class DocRevManager(object):
 
     def new_line(self):
         print("\n")
-        print("\n")
 
-    def replace_name(self, name_str):
+    def replace_name(self, name_str, file_type):
         if name_str.rsplit(".")[0][-1] == "1":
             self.skipped_files.append(name_str)
             return None 
         else:
             lstr = name_str.split(' Rev')[0]
-            return lstr + self.rev_str
+            return lstr + self.rev_str + "." + file_type
 
     def handle_doc(self, name_str, f_path, process=False):
-        file_type = name_str.rsplit(".")[1].lower()
-        replaced = False
-        
+        file_type = name_str.rsplit(".")[1].lower()        
         if file_type in self.delete_file_types:
             self.deleted_files.append(name_str)
             if process==True:
-                os.remove(os.path.join(name_str, f_path))
+                os.remove(os.path.join(f_path, name_str))
         else:
-            result = self.replace_name(name_str)
+            result = self.replace_name(name_str, file_type)
             if result is not None:
-                self.renamed_files.append(result)
+                self.target_files.append(result)
                 if process==True:
                     try:
-                        os.rename(os.path.join(f_path, name_str), os.path.join(f_path, new_name))
+                        os.rename(os.path.join(f_path, name_str), os.path.join(f_path, result))
+                        self.renamed_files.append(result)
                     except FileExistsError:
                         self.skipped_files.append(name_str)
                     except Exception as e:
-                        print(e)
+                        self.errors.append(e)
 
     def loop_dirs(self, process=False):
         self.reset_config()
         for sub_dir in os.listdir(self.top_dir):
             sub_dir_path = os.path.join(self.top_dir,sub_dir)
             for file_name in os.listdir(sub_dir_path):
-                self.handle_doc(file_name, sub_dir_path)
+                self.handle_doc(file_name, sub_dir_path, process=process)
                 self.count+=1
 
     def set_top_dir(self):
         self.new_line()
         print('Please input the top level directory or press "d" for the default.')
+        print('The current default is: {}'.format(self.top_dir))
         top_dir = input()
         if top_dir == "d":
             pass
@@ -72,6 +73,7 @@ class DocRevManager(object):
     def log_results(self):
         self.new_line()
         total_handled = len(self.renamed_files) + len(self.deleted_files)
+        print("RESULTS:")
         print("{} total file names modified to {}".format(len(self.renamed_files), self.rev_str))
         print("{} files deleted".format(len(self.deleted_files)))
         print("{} files skipped".format(len(self.skipped_files)))
@@ -79,16 +81,21 @@ class DocRevManager(object):
             print("OK. All files handled.")
         else: 
             print("Some files were not handles or deleted correctly")
+        if len(self.errors) != 0:
+            print("The following errors occured")
+            for e in self.errors:
+                print(e)
 
     def confirm(self):
         self.new_line()
         total_handled = len(self.renamed_files) + len(self.deleted_files)
         print("{} total files.".format(self.count))
-        print("{} total file names to be modified to {}.".format(len(self.renamed_files), self.rev_str))
         print("{} files names will be skipped".format(len(self.skipped_files)))
+        for skipped_file in self.skipped_files:
+            print(skipped_file)
         self.new_line()
-        print("The final file names will be the following:")
-        for name in self.renamed_files:
+        print("{} total file names to be modified to {}:".format(len(self.target_files), self.rev_str))
+        for name in self.target_files:
             print(name)
         self.new_line()
         print("{} files will be deleted".format(len(self.deleted_files)))
